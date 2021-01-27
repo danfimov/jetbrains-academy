d0457e9eb50048c0dab93487f52078edb27ae0d9
LoveSolaristics <lovesolaristics@gmail.com>
2021-01-27 22:28:01 +0300

    Add 2/6 stage of project

---
+++ Python Developer/Text-Based Browser/Problems/What day is it?/main.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+if n < -10:
+    print('Monday')
+elif n < 14:
+    print('Tuesday')
+else:
+    print('Wednesday')
--- Python Developer/Text-Based Browser/Text-Based Browser/task/browser/browser.py
+++ Python Developer/Text-Based Browser/Text-Based Browser/task/browser/browser.py
@@ -1,3 +1,5 @@
+import os
+import argparse
 
 nytimes_com = '''
 This New Liquid Is Magnetic, and Mesmerizing
@@ -34,11 +36,34 @@ Twitter and Square Chief Executive Officer Jack Dorsey
  Tuesday, a signal of the strong ties between the Silicon Valley giants.
 '''
 
-# write your code here
-while (user_input := input()) != 'exit':
-    if user_input == 'bloomberg.com':
-        print(bloomberg_com)
-    elif user_input == 'nytimes.com':
-        print(nytimes_com)
+VALIDATED_URLS = ['nytimes.com', 'bloomberg.com']
+
+
+def valid_url(check_url: str):
+    if "." in check_url and check_url in VALIDATED_URLS:
+        return True
     else:
-        print('Incorrect text, please try again!')
+        return False
+
+
+def save_content(file_content: str, file_directory: str, file_name: str):
+    path = os.path.join(file_directory, file_name)
+    with open(path, 'w') as file:
+        file.write(file_content)
+
+
+parser = argparse.ArgumentParser('This is a simple web-browser')
+parser.add_argument('dir', default='')
+
+directory = parser.parse_args().dir
+if not os.access(directory, os.F_OK):
+    os.mkdir(directory)
+
+while (user_input := input()) != 'exit':
+    if valid_url(user_input):
+        content = locals().get(user_input.replace(".", "_"))
+        print(content)
+        name = user_input.rpartition(".")[0]
+        save_content(content, directory, name)
+    else:
+        print("Error: Incorrect command or URL")
